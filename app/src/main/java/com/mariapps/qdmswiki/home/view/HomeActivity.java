b204ba03fe07dd803d65012859baf41536d2d31b
vaisakh.mohandas <ysakh15@gmail.com>
2020-06-11 10:26:23 +0530

    new commit

--- app/src/main/java/com/mariapps/qdmswiki/home/view/HomeActivity.java
+++ app/src/main/java/com/mariapps/qdmswiki/home/view/HomeActivity.java
@@ -281,8 +281,8 @@ public class HomeActivity extends BaseActivity implements HomeView {
         Log.e("allocated00", "" + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
        // Decompress decompress = new Decompress(Environment.getExternalStorageDirectory() + "/QDMSWiki/" + "20200527080434.zip", Environment.getExternalStorageDirectory() + "/QDMSWiki/ExtractedFiles");
         //decompress.execute();
-       ReadAndInsertJsonData readAndInsertJsonData = new ReadAndInsertJsonData();
-       readAndInsertJsonData.execute();
+       //ReadAndInsertJsonData readAndInsertJsonData = new ReadAndInsertJsonData();
+       //readAndInsertJsonData.execute();
         //setup();
         //5a0c0ade3b6a9e5490d6e7d0
     }
@@ -544,7 +544,7 @@ public class HomeActivity extends BaseActivity implements HomeView {
         Intent intent = new Intent(context, DownloadService.class);
         intent.putExtra("url", url);
         intent.putExtra("filename", zipFileName);
-       /* if (!url.equals("") && !zipFileName.equals("")) {
+        if (!url.equals("") && !zipFileName.equals("")) {
             if (!isMyServiceRunning(DownloadService.class)) {
                 Log.e("service", "notrunning");
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -555,7 +555,7 @@ public class HomeActivity extends BaseActivity implements HomeView {
             } else {
                 Log.e("service", "isrunning");
             }
-        }*/
+        }
     }
 
 
@@ -1463,10 +1463,18 @@ request.setAllowedNetworkTypes(
     public List<FileListModel> readJsonStream(InputStream in) throws IOException {
         JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
         List<FileListModel> messages = new ArrayList<FileListModel>();
-        reader.beginArray();
+        reader.beginObject();
         while (reader.hasNext()) {
-            FileListModel message = new Gson().fromJson(reader, FileListModel.class);
-            messages.add(message);
+            String name= reader.nextName();
+            if(name.equals("fileChunks")){
+                reader.beginArray();
+                while (reader.hasNext()){
+                    FileListModel message = new Gson().fromJson(reader, FileListModel.class);
+                    messages.add(message);
+                }
+            }
+
+
         }
         reader.endArray();
         reader.close();
